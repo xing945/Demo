64443030547dc3f3856ed2e3c91714ebb1d42fd4
xing <642743457@qq.com>
2020-08-08 13:18:26 +0800

    数字查找特效

--- 数字查找特效/src/util/isPrime.js
+++ 数字查找特效/src/util/isPrime.js
@@ -1,17 +1,15 @@
 /**
  * 判断是否是素数
- * @param {*} number 
+ * @param {*} number
  */
-export default function(number){
-    let flag = true;
-    for(let i = 2; i < number - 1; i++){
-        if(number % i === 0){
-            flag = false;
-            continue;
-        }
-    }
-    if(flag){
-        return true;
-    }
-    return false;
+export default function (number) {
+	if (number < 2) {
+		return false
+	}
+	for (let i = 2; i < number - 1; i++) {
+		if (number % i === 0) {
+			return false
+		}
+	}
+	return true
 }
